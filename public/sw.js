762da6e6741fb1f2173344b3a2e885ce9014726d
CartCostaVasques <cartcostavasques@gmail.com>
2026-03-26 12:13:14 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,14 @@
-// public/sw.js
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
+
+self.addEventListener('message', (event) => {
+  if (event.data?.type === 'SHOW_NOTIFICATION') {
+    self.registration.showNotification(event.data.titulo, {
+      body: event.data.corpo,
+      requireInteraction: false,
+    });
+  }
+});
 
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
